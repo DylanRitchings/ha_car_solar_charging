9218ef911fed85c179f2bf667ff63b7a3c62686c
DylanRitchings <DylanRitchings1998@gmail.com>
2024-08-28 21:20:19 +0100

    updates

--- main.py
+++ main.py
@@ -11,17 +11,10 @@ def watt(value: str):
 
 @dataclass
 class House:
-    INVERTER_MAX = kw(2.67)
-    SOLAR_MAX = kw(3.67)
-
-    solar_power: float
-    load_power: float
-    grid_consumption: float
-    battery_charge: float
-
-    # TODO maybe put options and charger in here?
-
     def __init__(self):
+        self.INVERTER_MAX = kw(2.67)
+        self.SOLAR_MAX = kw(3.67)
+
         self.solar_power = kw(sensor.foxess_solar_power)
         self.load_power = kw(sensor.foxess_load_power)
         self.grid_consumption = watt(
@@ -31,35 +24,31 @@ class House:
 
 @dataclass
 class Options:
-    battery_min: int  # %
-    charge_type: str
 
     def __init__(self):
-        self.charge_type = lower(input_select.car_charging)
+        self.charge_type = input_select.car_charging
         self.battery_min = 30
 
 
 @dataclass
 class Charger:
-    CHARGE_VOLTS = 230
-
-    load_power: float
-    available_current: float
-
-    house_id: str = "e658d337f118a91c07ecc90b1482f639"  # move to state
-    charger_id: str = "78ca94b33412b017cf3b53429b9a0b49"  # move to state
 
     def __init__(self):
+        self.CHARGE_VOLTS = 230
+        self.house_id = "e658d337f118a91c07ecc90b1482f639"  # move to state
+        self.charger_id = "78ca94b33412b017cf3b53429b9a0b49"  # move to state
         self.load_power = max(0, float(sensor.car_charging_kw))
-        self.available_current = int(
-            float(state.get("number.28_oriel_road_available_current")))
+        self.available_current = float(
+            state.get("number.28_oriel_road_available_current"))
 
     def to_current(self, power):
         return watt(power)/self.CHARGE_VOLTS
 
     def set_power_limit(self, new_load_power):
+
         current = self.to_current(new_load_power)
         if current < 6:
+            self.set_current_limit(0)
             self.switch_charger("off")
             return ""
         elif self.available_current != current:
@@ -67,8 +56,13 @@ class Charger:
         self.switch_charger("on")
 
     def set_current_limit(self, current: float):
-        zaptec.limit_current(blocking=False, return_response=False,
-                             device_id=self.house_id, available_current=current)
+        log.warning(f"limit current: {int(current)}")
+        zaptec.limit_current(
+            blocking=False,
+            return_response=False,
+            device_id=self.house_id,
+            available_current=int(current)
+        )
 
     def switch_charger(self, on_off: str):
         switch.charger_charging = on_off
@@ -79,11 +73,11 @@ class Charger:
 
 
 @service
-def main():
+def sync_car_to_solar():
     options = Options()
     charger = Charger()
 
-    if options.charge_type == "off":
+    if options.charge_type == "Off":
         charger.switch_charger("off")
         return ""
 
@@ -107,12 +101,14 @@ def main():
         0, house.INVERTER_MAX - house_only_load_power)
 
     new_power = 0
-
+    log.warning("charge_type: " + options.charge_type)
     if options.charge_type == "Slow-Battery (Battery or Solar)" and bat_has_charge:
         new_power = max(available_battery_power, available_solar_power)
     elif options.charge_type == "Battery (Battery & Solar)" and bat_has_charge:
+
         new_power = min(available_solar_power+available_battery_power,
                         house.INVERTER_MAX)  # TODO or solar_max_power
+        log.warning(new_power)
     elif options.charge_type == "Solar" or ("Battery" in options.charge_type and not bat_has_charge):
         new_power = min(available_solar_power, solar_max_power)
     elif options.charge_type == "Grid":
