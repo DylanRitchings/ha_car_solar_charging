efa132090fb84bb1a7b384afd1aa2cbc0b67b227
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2024-09-07 19:01:06 +0100

    Update car.py

--- car.py
+++ car.py
@@ -1,101 +1,138 @@
+
+def kw(value: str):
+    return float(value)*1000
+
+
+def watt(value: str):
+    return float(value)
+
+
+class House:
+    def __init__(self):
+        self.INVERTER_MAX = kw(2.67)
+        self.SOLAR_MAX = kw(3.67)
+
+        self.solar_power = kw(sensor.foxess_solar_power)
+        self.load_power = kw(sensor.foxess_load_power)
+        self.grid_consumption = watt(
+            sensor.octopus_energy_electricity_23l3042499_2700008005722_current_demand)
+        self.battery_charge = float(sensor.foxess_bat_soc)
+
+
+class Options:
+    def __init__(self):
+        self.charge_type = input_select.car_charging
+        self.battery_min = 30
+
+
+class Charger:
+    def __init__(self):
+        self.CHARGE_VOLTS = 230
+        self.house_id = "e658d337f118a91c07ecc90b1482f639"  # move to state
+        self.charger_id = "78ca94b33412b017cf3b53429b9a0b49"  # move to state
+        self.load_power = max(0, float(sensor.car_charging_kw))
+        self.available_current = float(
+            state.get("number.28_oriel_road_available_current"))
+
+    def to_current(self, power):
+        return watt(power)/self.CHARGE_VOLTS
+
+    def set_power_limit(self, new_load_power):
+
+        current = self.to_current(new_load_power)
+        if current < 6:
+            self.set_current_limit(0)
+            self.switch_charger("off")
+            return ""
+        elif self.available_current != current:
+            self.set_current_limit(current)
+        self.switch_charger("on")
+
+    def set_current_limit(self, current: float):
+        log.warning(f"limit current: {int(current)}")
+        zaptec.limit_current(
+            blocking=False,
+            return_response=False,
+            device_id=self.house_id,
+            available_current=int(current)
+        )
+
+    def switch_charger(self, on_off: str):
+        switch.charger_charging = on_off
+        if on_off == "off":
+            zaptec.stop_charging(device_id=self.charger_id)
+        else:
+            zaptec.resume_charging(device_id=self.charger_id)
+
+
+class Calculations:
+
+    def __init__(self, house: House, charger: Charger, options: Options):
+        self.house_only_load_power = max(
+            0, house.load_power - charger.load_power)
+        self.available_solar_power = max(
+            0, house.solar_power - self.house_only_load_power)  # maybe put - 100
+        self.bat_has_charge = house.battery_charge > options.battery_min
+        self.solar_max_power = max(
+            0, house.SOLAR_MAX - self.house_only_load_power)
+        self.available_battery_power = max(
+            0, house.INVERTER_MAX - self.house_only_load_power)
+
+        self.slow_battery_power = max(
+            self.available_battery_power, self.solar_max_power)
+        self.fast_battery_power = min(
+            self.available_solar_power+self.available_battery_power, house.INVERTER_MAX)
+        self.solar_power = min(
+            self.available_solar_power, self.solar_max_power)
+        self.grid_power = kw(7.36)
+
+        self._set_states()
+
+    def _set_states(self):
+        for key, value in self.__dict__.items():
+            attributes = {
+                "device_class": "power",
+                "unit": "W",
+                "state_class": "measurement"
+            }
+            if key == "bat_has_charge":
+                attributes = {
+                    "device_class": "boolean"
+                }
+            if not key.startswith('_'):
+                state.set(var_name=f"dylscript.{key}",
+                          value=value, 	new_attributes=attributes)
+
+
+
 @service
 def sync_car_to_solar():
-    """sync car to solar"""
-    CHARGE_VOLTS = 230
-    INVERTER_MAX_KW = 2.67
-    SOLAR_MAX_KW = 3.67
-    charge_type=input_select.car_charging
-    
+    options = Options()
+    charger = Charger()
 
-    try:
-        log.warning(charge_type)
-        log.warning(automation.car_charging)
-        if charge_type == "Off":
-            log.warning("Turn charging off")
-            zaptec.stop_charging(device_id="78ca94b33412b017cf3b53429b9a0b49")
-            log.warning(automation.car_charging)
-            switch.charger_charging = "off"
-            return ""
+    if options.charge_type == "Off":
+        charger.switch_charger("off")
+        return ""
 
-            
-        solar_power = float(sensor.foxess_solar_power)
-        #log.warning(f"solar power: {solar_power}")
-        
+    house = House()
 
-        current_charge_power = max(0, float(sensor.car_charging_kw))
-        #log.warning(f"current charge power: {current_charge_power}")
-        before_load_power = float(sensor.foxess_load_power) - current_charge_power
-        load_power = max(0, before_load_power)
-        
-        #log.warning(f"before load power: {before_load_power}")
-        
-        #log.warning(f"foxess_load_power: {sensor.foxess_load_power}")
+    calc = Calculations(house, charger, options)
 
-        #TODO fix charging going to 0 due to load_power being taken away from solar_oower
-        available_solar = max(0, solar_power - load_power - 0.1)
-        #available_solar = max(0, solar_power - 0.1)
+    new_power = None
 
-        log.warning(f"available_solar: {available_solar}")
-        
-        grid_consumption = float(sensor.foxess_grid_consumption_power)
-        
-        #log.warning(f"grid_consumption: {grid_consumption}")
-        
-        bat_has_charge = float(sensor.foxess_bat_soc) > float(25)
-        
-            
-        max_power = max(0, SOLAR_MAX_KW - load_power - 0.1)
-        #max_power = max(0, SOLAR_MAX_KW)
-        #log.warning(f"max_power: {max_power}")
-        
-        available_bat_load = max(0, INVERTER_MAX_KW - load_power)
-        
-        if charge_type=="Slow-Battery (Battery or Solar)" and bat_has_charge:
-        
-            charge_kw = max(available_bat_load, available_solar)
-            
-        elif charge_type=="Battery (Battery & Solar)" and bat_has_charge:
-            
-            log.warning("BATTERY")
-            
-            
-            charge_kw = min(available_solar+available_bat_load, max_power)
-            #charge_kw = max(available_bat_load, available_solar)# - grid_consumption
-            #TODO add rest of solar to 2.7 to get actual value
-               
-        elif charge_type=="Solar" or ("Battery" in charge_type and not bat_has_charge):
-            charge_kw = min(available_solar, max_power)# - grid_consumption
-        elif charge_type=="Grid":
-            charge_kw = 7.36
-        elif charge_type=="6 Amps (Slowest)":
-            zaptec.limit_current(blocking=False, return_response=False, device_id="e658d337f118a91c07ecc90b1482f639", available_current=6)
-            return ""
-            
-        log.warning(f"charge_kw: {charge_kw}")
-        
-        charge_amps = (charge_kw*1000)/CHARGE_VOLTS
-        
-        charge_amps_floored = int(charge_amps)
-        
-        if charge_amps_floored < 6:
-            zaptec.stop_charging(device_id="78ca94b33412b017cf3b53429b9a0b49")
-            switch.charger_charging = "off"
+    # TODO change to hashmap?
+    if options.charge_type == "Slow-Battery (Battery or Solar)" and self.bat_has_charge:
+        new_power = calc.slow_battery_power
+    elif options.charge_type == "Battery (Battery & Solar)" and self.bat_has_charge:
+        new_power = calc.fast_battery_power
+    elif options.charge_type == "Solar" or ("Battery" in options.charge_type and not self.bat_has_charge):
+        new_power = calc.solar_power
+    elif options.charge_type == "Grid":
+        new_power = calc.grid_power
+    elif options.charge_type == "6 Amps (Slowest)":
+        charger.limit_current(6)
+        charger.switch_charger("on")
+        return ""
 
-        elif int(float(state.get("number.28_oriel_road_available_current"))) != charge_amps_floored:
-            #log.warning(f"charge amps: {charge_amps_floored}")
-            log.warning(f"limit current to: {charge_amps_floored} A")
-            zaptec.limit_current(blocking=False, return_response=False, device_id="e658d337f118a91c07ecc90b1482f639", available_current=charge_amps_floored)
-            zaptec.resume_charging(device_id="78ca94b33412b017cf3b53429b9a0b49")
-            switch.charger_charging = "on"
-        else:
-            log.warning("Turn charging on")
-            zaptec.resume_charging(device_id="78ca94b33412b017cf3b53429b9a0b49")
-            switch.charger_charging = "on"
-        
-
-        
-        #state.set("number.28_oriel_road_available_current", value=charge_amps_floored)
-    except Exception as e:
-        log.error(e)
-        #service.call(domain="zaptec", name="limit_current", available_current=0.0, device_id="e658d337f118a91c07ecc90b1482f639")
-        #zaptec.limit_current(blocking=False, return_response=False, device_id="e658d337f118a91c07ecc90b1482f639", available_current=0.0)
+    if new_power:
+        charger.set_power_limit(new_power)
